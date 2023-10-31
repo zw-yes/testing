6b623e0f324ac65794f012a5b4d2b11e26a32e4f
zw-yes <149354283+zw-yes@users.noreply.github.com>
2023-10-31 12:30:05 +0800

    Update script.js
    added ducks, cows

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const chicken_label = document.getElementById("chicken")
 const chicken_button = document.getElementById("buy_chicken")
+const duck_label = document.getElementById("duck")
+const duck_button = document.getElementById("buy_duck")
+const cow_label = document.getElementById("cow")
+const cow_button = document.getElementById("buy_cow")
 const money_label = document.getElementById("money")
 const money_per_second_label = document.getElementById("money_per_second")
 var money = 0
 var chicken_cost = 10
 var chicken = 1
+var duck_cost = 250
+var duck = 0
+var cow_cost = 1500
+var cow = 0
+
 var money_per_second = 2
 
 //cool sleep function
@@ -30,16 +39,42 @@ async function buy_chicken() {
   }
 }
 
+//purchase duck
+async function buy_duck() {
+  if (money >= duck_cost) {
+    duck += 1
+    money -= duck_cost
+    money_label.textContent = "Money > " + money
+    duck_label.textContent = "Ducks: " + duck
+    duck_cost += Math.round(Math.sqrt(duck_cost/1.5))
+    duck_button.textContent = "buy chicken($" + duck_cost + ")"
+    //scaling cost idk
+  }
+}
 
+//purchase cow
+async function buy_cow() {
+  if (money >= cow_cost) {
+    cow += 1
+    money -= cow_cost
+    money_label.textContent = "Money > " + money
+    cow_label.textContent = "cow: " + cow
+    cow_cost += Math.round(Math.sqrt(cow_cost/1.5))
+    cow_button.textContent = "buy cow($" + cow_cost + ")"
+    //scaling cost idk
+  }
+}
 
 //update money per second
 setInterval(function(){
-  money_per_second = (chicken*2)
+  money_per_second = (chicken*1) + (duck*10) + (cow*50)
   money_per_second_label.textContent = "Money/s > " + money_per_second
 }, 100)
 
-//chicken button thingie
+//animals button thingie
 chicken_button.addEventListener("click", buy_chicken)
+duck_button.addEventListener("click", buy_duck)
+cow_button.addEventListener("click", buy_cow)
 
 //update money
 setInterval(function(){
